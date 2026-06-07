9cb4e704f4ceaff4a5b8eca0c18c8960f12a9270
Amanda Santos <santos.amandams@gmail.com>
2026-06-07 19:38:20 -0300

    ajuste log

--- springboot-pay-split/src/main/java/com/example/springboot_pay_split/ValidacaoSkipListener.java
+++ springboot-pay-split/src/main/java/com/example/springboot_pay_split/ValidacaoSkipListener.java
@@ -1,13 +1,14 @@
 package com.example.springboot_pay_split;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.SkipListener;
 
+@Slf4j
 public class ValidacaoSkipListener implements SkipListener<Transaction, Transaction> {
 
     @Override
     public void onSkipInProcess(Transaction item, Throwable t) {
-        System.err.println("==> ITEM INVALIDO PULADO: ID " + item.externalId() +
-                " | Motivo: " + t.getMessage());
+        log.warn("==> ITEM INVALIDO PULADO: ID {} | Motivo: {}", item.externalId(), t.getMessage());
     }
 
     @Override
